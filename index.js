df349b5c242544c375151dee1d2f6dea4f248552
Christopher McElvogue <chris.mcelvogue@gmail.com>
2021-12-18 15:29:14 -0800

    commit tree b1553a779ceedfab22e857b3fac90fa5b0e02676

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
 // Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-  }
-  return gifts;
-}
-
-wrapGifts(gifts);
-
-function writeCards(names, occasion) {
-  let newArray = [];
+function writeCards(names, event) {
+  let thankYouCards = []
   for (let i = 0; i < names.length; i++) {
-    newArray.push(
-      `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`
-    );
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
-  return newArray;
+  return thankYouCards
 }
 
-writeCards(["Lisa", "Kaitlin", "Jan"], "suprise");
-
-function countDown(num) {
-  while (num >= 0) {
-    console.log(num--);
+function countDown(start) {
+  while (start >= 0) {
+    console.log(start)
+    start--
   }
 }
